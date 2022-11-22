cb61c7e1d64a1d158a62f7a15aae7e3a0c0dd506
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2022-11-22 17:07:44 +0100

    create/index modification
    Modified also styles on all views - same buttons etc.

--- AKP_TrackManager/Models/CarMember.cs
+++ AKP_TrackManager/Models/CarMember.cs
@@ -7,9 +7,9 @@ namespace AKP_TrackManager.Models
 {
     public partial class CarMember
     {
+        public int CarMemberId { get; set; }
         public int MemberMemberId { get; set; }
         public int CarCarId { get; set; }
-        public int CarMemberId { get; set; }
 
         public virtual Car CarCar { get; set; }
         public virtual Member MemberMember { get; set; }
--- AKP_TrackManager/Models/DTO/LoginCredentials.cs
+++ AKP_TrackManager/Models/DTO/LoginCredentials.cs
@@ -1,8 +1,12 @@
-﻿namespace AKP_TrackManager.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AKP_TrackManager.Models.DTO
 {
     public class LoginCredentials
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
